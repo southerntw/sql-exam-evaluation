dec8459885e9f707b7babfc44dc967346c6db3f0
southerntw <fauzanmansur.a@gmail.com>
2023-12-03 01:10:25 +0800

    Added new models

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -4,6 +4,11 @@ const db = require('../config/Database.js')
 const { DataTypes } = Sequelize;
 
 const Users = db.define('users',{
+    userId: {
+        type: DataTypes.INTEGER,
+        autoIncrement = true,
+        primaryKey = true
+    }
     name:{
         type: DataTypes.STRING
     },
@@ -13,11 +18,75 @@ const Users = db.define('users',{
     password:{
         type: DataTypes.STRING
     },
+    role: {
+        type: DataTypes.STRING
+    },
+    group: {
+        type: DataTypes.INTEGER
+    },
+    score: {
+        type: DataTypes.INTEGER
+    }
     refresh_token:{
         type: DataTypes.TEXT
     }
-},{
+},
+{
     freezeTableName:true
+});
+
+const Groups = db.define('groups', {
+    groupId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    }
+});
+
+const Questions = db.define('questions', {
+    questionId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    groupId: {
+        type: DataTypes.INTEGER
+    },
+    text: {
+        type: DataTypes.TEXT
+    },
+    difficulty: {
+        type: DataTypes.STRING
+    },
+    creatorId: {
+        type: DataTypes.INTEGER
+    },
+    correctAnswer: {
+        type: DataTypes.TEXT // TODO: Requires correction
+    }
+});
+
+const Answers = db.define('answers', {
+    answerId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    userId: {
+        type: DataTypes.INTEGER
+    },
+    groupId: {
+        type: DataTypes.INTEGER
+    },
+    questionId: {
+        type: DataTypes.INTEGER
+    },
+    answer: {
+        type: DataTypes.TEXT // TODO: Requires correction
+    },
+    isCorrect: {
+        type: DataTypes.BOOLEAN
+    }
 });
 
 (async () => {
